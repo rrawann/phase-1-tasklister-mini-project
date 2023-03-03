6bafa2a0043093dc6adffa0e14f7f9378581eb82
Rawan <rawnq.sb@gmail.com>
2023-03-03 12:11:11 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.new_task_description.value)
+  })
+
 });
+
+  function createToDo(todo, todo2) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} - ${todo2} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
